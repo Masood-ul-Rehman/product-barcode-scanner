006a75e18d7795fd6089d51531d94d713e7674ee
Masood-ul-Rehman <81099787+Masood-ul-Rehman@users.noreply.github.com>
2023-06-13 22:57:19 +0500

    completed store page

--- lib/Provider/dataProvider.dart
+++ lib/Provider/dataProvider.dart
@@ -2,11 +2,24 @@ import 'package:flutter/material.dart';
 
 class DataProvider with ChangeNotifier {
   dynamic _data;
-
+  dynamic _store;
+  dynamic _storeName;
   dynamic get data => _data;
+  dynamic get store => _store;
+  dynamic get storeName => _storeName;
 
   void setData(dynamic newData) {
     _data = newData;
+    notifyListeners();
+  }
+
+  void setStore(dynamic id) {
+    _store = id;
+    notifyListeners();
+  }
+
+  void setStoreName(dynamic name) {
+    _storeName = name;
     notifyListeners();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        primaryColor: Colors.deepPurple,
       ),
       home: const LoginScreen(),
     );
